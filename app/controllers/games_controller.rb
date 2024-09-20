8b012e7933d840b846f9e363c10c50a25b991824
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2024-09-20 20:26:49 +0200

    game controller action play

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,6 +21,9 @@ class GamesController < ApplicationController
   def show
     @player = Player.new
     @users = current_user.all_friends + [current_user]
+    User.joins(:players).where(players: { game_id: @game.id }).each do |user|
+      @users.delete(user)
+    end
     @players = (1..[@game.player_count, 5].min).map do |player_id|
       @game.players.find_by(player_id:) || Player.new
     end
@@ -40,14 +43,20 @@ class GamesController < ApplicationController
     @game.start = Time.now
     create_game_cards
     if @game.save
-      redirect_to play_path(@game)
+      redirect_to play_path(@game), alert: 'game started successfully'
     else
       redirect_to @game, alert: 'game could not be started'
     end
   end
 
   def play
+    @me = Player.find_by(user: current_user)
+    # assign instance variables for the other players
     @players = @game.players
+    @players.each do |player|
+      instance_variable_set("@player#{player.id}", player) if player != @me
+    end
+    raise
   end
 
   def end
@@ -67,15 +76,15 @@ class GamesController < ApplicationController
   def add_cards(card_ids, count)
     count.times do
       card_ids.each do |card_id|
-        @cards << GameCard.create!(game: @game, card_id:, position: 1)
+        @cards << GameCard.create!(game: @game, card_id:, position: 6)
       end
     end
   end
 
   def create_game_cards
     @cards = []
-    add_cards([0, 13], 2)
-    add_cards((1..12).to_a, 4)
+    add_cards([1, 14], 2)
+    add_cards((2..13).to_a, 4)
     @cards.shuffle!
   end
 end
